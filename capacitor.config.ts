f395284b9d8c03328f16d92c349cd843b323e284
aryaadhi <78729692+aryaadhi@users.noreply.github.com>
2022-11-05 20:35:20 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'helpdeskApp',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
